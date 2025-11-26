50d8c19c3b4c313aa2f823c79e3e7e855074a2ce
vijay <146230287+Vijayvmu@users.noreply.github.com>
2025-11-26 17:39:26 +0530

    Add files via upload

--- calculator/cal.js
+++ calculator/cal.js
@@ -14,7 +14,6 @@ for(let i=4;i<=6;i++){
     htmlbutton2 += `<button onclick="num(${i})">${i}</button>`
 }
 button2.innerHTML = htmlbutton2;
-
 //div3
 for(let i=7;i<=9;i++){
     htmlbutton3 += `<button onclick="num(${i})">${i}</button>`
@@ -22,7 +21,6 @@ for(let i=7;i<=9;i++){
 button3.innerHTML = htmlbutton3;
 
 const  display=document.getElementById("display");
-
 /* create an functions for display numbers when key presses in the device */
 document.addEventListener("keydown", function (event) {
     const key = event.key;
@@ -38,7 +36,6 @@ document.addEventListener("keydown", function (event) {
     event.preventDefault();
 });
 
-
 /* create an functions for display the number */
 function num(value){
     display.value += value;
@@ -52,7 +49,6 @@ function operator(value){
 function Clear(){
     display.value ="";
 }
-
 
 function submit(){
     try{
